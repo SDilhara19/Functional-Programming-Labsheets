03d8b83411fdb6d6dd588a7485f3b04199879dd0
SDilhara19 <125456493+SDilhara19@users.noreply.github.com>
2023-08-15 10:08:53 +0530

    Q3.scala

--- Tutorial8/Q3.scala
+++ Tutorial8/Q3.scala
@@ -2,28 +2,22 @@ object Q3 {
   val toUpper: (String) => String = x => x.toUpperCase()
   val toLower: (String) => String = x => x.toLowerCase()
 
-  val NameFormat: ((String) => String, String) => Unit = (f, x) => {
-    val r = f(x)
-    println(r)
-  }
 
-  def toUppcer(x: String, index: Int): String = {
+  val toCharUpper: (String, Int) => String = (x, index) => {
     val newString = new StringBuilder(x)
     val char = newString(index)
     newString(index) = char.toUpper
     newString.toString()
   }
 
-  def main(args: Array[String]): Unit = {
-    NameFormat(toUpper(_, 5), "Sanduni") // Using partial application for toUpper
-    println(toUpper("Sanduni", 5)) // Calling toUpper directly
-  }
+  val FormatName1: ((String) => String, String) => Unit = (f, x) => println(f(x))
 
-
-
+  val FormatName2: ((String, Int) => String, String, Int) => Unit = (f, x, y) => println(f(x,y))
 
 def main(args: Array[String]): Unit = {
-    NameFormat(toUpper, "Sanduni")
-    toUppcer("Sanduni", 5)
+  FormatName1(toUpper, "Benny")
+  FormatName2(toCharUpper, "Niroshan", 1)
+  FormatName1(toLower, "saman")
+  FormatName2(toCharUpper, "Kumara", 5)
   }
 }
